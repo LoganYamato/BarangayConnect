34a2e383e53b1b489fed86fe11f13a00dedc3336
LoganYamato <61789100+LoganYamato@users.noreply.github.com>
2025-10-27 23:40:01 +0800

    Add Firebase configuration and initialization

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// --- Firebase Setup ---
+// Import Firebase modules from CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6f9Fo1aDebjEurvWFsffM33izUPUTylw",
+  authDomain: "iss-bc.firebaseapp.com",
+  projectId: "iss-bc",
+  storageBucket: "iss-bc.firebasestorage.app",
+  messagingSenderId: "455122393981",
+  appId: "1:455122393981:web:bdf281da744767c0064a14",
+  measurementId: "G-6VQLV0PG81"
+};
+
+// Initialize Firebase and Firestore
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
